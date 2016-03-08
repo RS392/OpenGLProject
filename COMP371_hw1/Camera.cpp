edb8926dca03c8645da875e4e4308d3dd6db4c4b
Clément Hennebelle <clement.hennebelle@hotmail.fr>
2016-03-08 12:07:38 -0500

    Fuck Source Tree.

--- COMP371_hw1/Camera.cpp
+++ COMP371_hw1/Camera.cpp
@@ -2,20 +2,11 @@
 #include "Camera.h"
 
 
-Camera::Camera()
+Camera::Camera(const glm::mat4 &view_matrix)
 {
+	this->view_matrix = view_matrix;
 }
-/*
-vec3 Camera::getCameraPos() {
-	int viewport[4];
-	// get matrixs and viewport:
-	glGetDoublev(GL_MODELVIEW_MATRIX, matModelView);
-	glGetDoublev(GL_PROJECTION_MATRIX, matProjection);
-	glGetIntegerv(GL_VIEWPORT, viewport);
-	gluUnProject((viewport[2] - viewport[0]) / 2, (viewport[3] - viewport[1]) / 2,
-		0.0, matModelView, matProjection, viewport,
-		&camera_pos[0], &camera_pos[1], &camera_pos[2]);
-}*/
+
 Camera::~Camera()
 {
 }
--- COMP371_hw1/Camera.h
+++ COMP371_hw1/Camera.h
@@ -1,8 +1,13 @@
 #pragma once
 class Camera
 {
+private:
+	glm::mat4 view_matrix;
+
 public:
-	Camera();
+	Camera(const glm::mat4 &view_matrix);
 	~Camera();
+
+	void keyPressed(GLFWwindow *_window, int key, int scancode, int action, int mods);
 };
 
